a516b973009f18d2ed2b6ee8a486f9a9f8d8db5c
Lenok <eseredich8@gmail.com>
2024-05-22 20:23:44 +0300

    init: add files

---
+++ gulpfile.js
@@ -0,0 +1,41 @@
+import gulp from 'gulp'
+import browserSync from 'browser-sync'
+import dartSass from "sass"
+import gulpSass from "gulp-sass"
+import rename from 'gulp-rename'
+// import sass from 'sass'
+
+const sass = gulpSass(dartSass);
+
+import cleanCSS from 'gulp-clean-css'
+import autoprefixer from 'gulp-autoprefixer'
+
+gulp.task('server', function() {
+    browserSync.init({
+        server: {
+            baseDir: "src"
+        }
+    });
+
+    // gulp.watch("src/*.html").on('change', browserSync.reload);
+});
+
+gulp.task('styles', function() {
+    return gulp.src("src/sass/*.+(scss|sass)")
+        .pipe(sass({outputStyle: 'expanded'})).on('error', sass.logError)
+        .pipe(rename({suffix: '.min', prefix: ''}))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(gulp.dest("src/css"))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('watch', function() {
+    gulp.watch("src/sass/**/*.+(scss|sass)", gulp.parallel('styles'));
+    // gulp.watch("src/*.html").on("change", browserSync.reload);
+})
+
+gulp.task('default', gulp.parallel('watch', 'server', 'styles'));
