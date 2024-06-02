de43cc89e671767d368f388045dbac67f0059b68
Lenok <eseredich8@gmail.com>
2024-06-02 22:20:30 +0300

    feat: validate html

--- src/js/script.js
+++ src/js/script.js
@@ -39,4 +39,59 @@ $(document).ready(function(){
 
     toggleSlide('.catalog-item__link');
     toggleSlide('.catalog-item__back');
+
+    // Modal #consultation, #order, #thanks, .overlay
+    $('[data-modal="consultation"]').on('click', function(){
+        $('.overlay, #consultation').fadeIn('slow');
+    });
+    $('.modal__close').on('click', function(){
+        $('#consultation, #order, #thanks, .overlay').fadeOut('slow');
+    })
+    // $('.button_mini').on('click', function(){
+    //     $('.overlay, #order').fadeIn('slow');
+    // });
+
+    $('.button_mini').each(function(i){
+        $(this).on('click', function(){
+            $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+            $('.overlay, #order').fadeIn('slow');
+        })
+    })
+
+    // form validate
+    function valideForms(form){
+        $(form).validate({
+            rules:{
+                name: 'required',
+                phone: 'required',
+                email: {
+                    required: true,
+                    email: true
+                }
+            }
+        });
+    }
+
+    valideForms('#consultation form');
+    valideForms('#order form');
+    valideForms('#consultation-form');
+    
+    // $('input[name=phone]'){
+    //     $("#phone").mask("(999) 999-9999");
+    // };
+
+    // smooth scroll adn pageup
+    // $(window).scroll(function(){
+    //     if($(this).scrollTop() < 800){
+    //         $(',pageup').fadeIn();
+    //     } else{
+    //         $('.pageup').fadeOut();
+    //     }
+    // })
+
+    $("a[href^='#"),click(function(){
+        let _href = $(this).attr("href");
+        $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+        return false;
+    })
 })
